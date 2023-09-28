d1c07e49e6be71f0c6936c215ccf36c4e811db62
DeclanTurnbaugh <declantcollege@gmail.com>
2023-09-27 21:33:20 -0600

    Final commit

---
+++ index.js
@@ -0,0 +1,14 @@
+const customer = {
+  firstName: "jake",
+  lastName: "smith",
+  email: "Jak3Smith1992@email.com",
+  phone: 3195551234,
+  favoriteFlavors: ["coffee", "strawberry", "matcha"],
+  cupSize: "medium",
+  firstVisit: false,
+  toppings: ["chocolate sprinkles", "wafer straws", "gummy bears"],
+  futureFlavors: "mango",
+  todaysPurchaseCost: 5.32,
+};
+
+console.log(customer);
